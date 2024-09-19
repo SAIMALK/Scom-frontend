d2654437c088f275a0b9e387def4118f73bbf7ac
SAIMALK <aleesasaim496@gmail.com>
2024-09-19 07:56:47 +0500

    v

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ const router = createBrowserRouter(
         <Route path='/profile' element={<ProfileScreen />} />
       </Route>
       {/* Admin users */}
-      <Route path='' element={<AdminRoute />}>
+      <Route path='' element={<PrivateRoute />}>
         <Route path='/admin/orderlist' element={<OrderListScreen />} />
         <Route path='/admin/productlist' element={<ProductListScreen />} />
         <Route
