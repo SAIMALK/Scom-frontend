36af53c2a85adf7a2e46bf518028a9b35f58115a
SAIMALK <115073479+SAIMALK@users.noreply.github.com>
2024-10-24 19:26:43 +0500

    Update apiSlice.js

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../constants';
 import { logout } from './authSlice';
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, 
-  credentials: 'include' });
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
 console.log(BASE_URL)
   
